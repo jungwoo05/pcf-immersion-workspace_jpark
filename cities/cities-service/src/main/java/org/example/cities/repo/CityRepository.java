9b5da9d9cc9f4fad74c8d9cbf2a704ecd617600f
unknown <parkj41@KOGSPARKJ41L1C.corp.emc.com>
2015-04-23 12:37:00 -0700

    modified

--- cities/cities-service/src/main/java/org/example/cities/repo/CityRepository.java
+++ cities/cities-service/src/main/java/org/example/cities/repo/CityRepository.java
@@ -1,6 +1,7 @@
 package org.example.cities.repo;
 
 import org.example.cities.model.City;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
